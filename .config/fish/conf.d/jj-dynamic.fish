2d1aff3217972a47246e73f73699615a7687e293
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-12-08 15:16:37 -0800

    Load jj-dynamic dynamically

--- .config/fish/conf.d/jj-dynamic.fish
+++ .config/fish/conf.d/jj-dynamic.fish
@@ -1,1 +1,3 @@
-../../../.local/config_modules/fish/jj_completions_bnjmtt4n/jj.fish
+if not set -q JJ_NEW_COMPLETIONS
+    source ~/stow/submodules/.local/config_modules/fish/jj_completions_bnjmtt4n/jj.fish
+end
